9a819b9884a49b19a4ba958f629bb42cad922738
jimlemoine <jlemoine007@gmail.com>
2021-11-09 14:35:58 -0800

    added jsonwebtoken and completed users-model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,6 +1,9 @@
 const db = require('../../data/db-config.js');
 
 function find() {
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
@@ -21,6 +24,10 @@ function find() {
 }
 
 function findBy(filter) {
+  return db('users as u')
+    .join('roles as r', 'u.role_id', 'r.role_id')
+    .select('user_id', 'username', 'role_name', 'password')
+    .where(filter);
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
@@ -37,6 +44,11 @@ function findBy(filter) {
 }
 
 function findById(user_id) {
+  return db('users as u')
+    .join('roles as r', 'u.role_id', 'r.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('user_id', user_id)
+    .first();
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
